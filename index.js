61ef5508b0e44375e64594eba65a9abaaf7d1e88
Naty02 <nataliegoncalvesdasilva02@gmail.com>
2022-06-18 16:56:35 -0300

    exercício trocaimg

---
+++ index.js
@@ -0,0 +1,13 @@
+function mudarImagem() {
+    var text = window.document.getElementById('texto');
+    text.style.fontSize = '30px';
+    text.innerText = "OLHA QUE MARAVILHOSA!!";
+    text.style.transition = "all 1.7s";
+    var imagem = window.document.getElementById('imagem');
+    imagem.src = "troca2.png";
+    imagem.style.border = "2px solid black"
+    var cor = window.document.getElementById('container');
+    cor.style.backgroundColor = 'rgb(121, 6, 17)';
+    cor.style.transition = "all 1.7s";
+
+}
